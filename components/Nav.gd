38c81ec30f7dc242b9d6b1dbdf41d8815d47f09e
molarmanful <benjinpang@gmail.com>
2024-10-09 12:09:26 -0400

    nice

--- components/Nav.gd
+++ components/Nav.gd
@@ -1,10 +1,12 @@
 extends PanelContainer
 
 @export var node_theme: CheckButton
+@export var font_name: Label
 
 
 func _ready() -> void:
 	node_theme.toggled.connect(ontoggle)
+	font_name.text = StateVars.font.family
 
 
 func ontoggle(on: bool) -> void:
