c40c0f073c2e20490bfeb11e494884533e0b8bb4
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-10-28 01:43:24 -0400

    (prog) validation

--- components/settings/font/id.gd
+++ components/settings/font/id.gd
@@ -1,6 +1,25 @@
 extends LineEdit
 
 
+func _ready() -> void:
+	text_changed.connect(validate)
+
+
+# TODO: potentially separate into another validator
+func validate(new: String) -> String:
+	var r = RegEx.new()
+	r.compile("[^\\w]")
+	var old_len = text.length()
+	var old_caret = caret_column
+	print(caret_column)
+	text = r.sub(new.to_lower(), "", true)
+	caret_column = old_caret + text.length() - old_len
+
+	if text.is_empty():
+		return "id cannot be empty"
+	return ""
+
+
 func save() -> void:
 	StateVars.font.id = text
 
