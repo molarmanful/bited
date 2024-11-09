58682af31848bbb2d8376dee776203825b6d5e9e
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-11-08 23:01:03 -0500

    ibm codepages

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,17 +1,17 @@
 (async () => {
-  const $ = "Thai";
-  const id = `macos${$.toLowerCase()}`;
-  const name = `Mac OS ${$}`;
-  const category = "Mac OS";
+  const $ = "1026";
+  const id = `ibm${$.padStart(5, "0")}`;
+  const name = `IBM-${$}`;
+  const category = "IBM";
   const res = await fetch(
-    `https://www.unicode.org/Public/MAPPINGS/VENDORS/APPLE/${$.toUpperCase()}.TXT`,
+    `https://raw.githubusercontent.com/kreativekorp/bitsnpicas/refs/heads/master/main/java/BitsNPicas/src/com/kreative/unicode/mappings/${id.toUpperCase()}.txt`,
   );
   const x = await res.text();
 
+  const uniq = new Set();
   const ascii = new Map([...Array(128).keys()].map((x) => ["c" + x, x]));
-  let map = new Map([
-    ...ascii,
-    ...x
+  let map = new Map(
+    x
       .split("\n")
       .filter(
         (a) =>
@@ -22,9 +22,11 @@
       )
       .map((a) => {
         const [k, v] = a.split(/\s+/);
+        uniq.add(+v);
         return ["c" + +k, +v];
       }),
-  ]);
+  );
+  if (map.size != uniq.size) console.log("WARN: NOT UNIQ");
   //map = new Map(
   //  [
   //    ..."；∧∨“”⊞⊟➙⧴″¶‴＆｜↶↷⟲←↑→↓⎇‽↧↥⌊⌈±↖↗↘↙ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~⭆？⪫⪪℅◧◨⮌≡№⊙⸿⬤≔≕▷▶✂ΣΠ↨⍘✳↔≦≧ⅈⅉ⌕⊕⊖⊗⊘⎚¬₂Φ§﹪ææ«»×⁺æ⁻·÷⁰¹²³⁴⁵⁶⁷⁸⁹¦‖‹⁼›¿æＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ⟦∕⟧æ…´αβχδεφγηιæκλμν¤πθρστυςωξψζ⦃æ⦄～æ",
