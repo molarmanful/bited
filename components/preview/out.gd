f7fd03164675bf1f226a2b8bd008d9f33c91d5c9
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-11-12 03:56:24 -0500

    font preview

---
+++ components/preview/out.gd
@@ -0,0 +1,74 @@
+class_name PreviewOut
+extends Node2D
+
+@export var view: SubViewport
+
+var text := ""
+var cache := {}
+
+
+func _draw() -> void:
+	var pos := Vector2i(0, StateVars.font.asc)
+	var mx := Vector2i.ZERO
+	for line in text.split("\n"):
+		for c in line:
+			var uc := c.unicode_at(0)
+			if uc not in cache:
+				continue
+
+			var q: Dictionary = cache[uc]
+			var dwidth: int = StateVars.font.dwidth if q.dwidth < 0 else q.dwidth
+			if q.tex:
+				draw_texture(
+					q.tex,
+					pos + Vector2i(q.off_x, -q.bb_y - q.off_y),
+				)
+			pos.x += dwidth
+
+		mx.x = max(mx.x, pos.x + StateVars.font.bb.x)
+		pos.x = 0
+		pos.y += StateVars.font.bb.y
+		mx.y = pos.y
+
+	view.size = mx
+
+
+func refresh(hard := false) -> void:
+	if hard:
+		cache.clear()
+
+	var ucs := {}
+	for c in text:
+		var uc := c.unicode_at(0)
+		if uc not in cache:
+			ucs[uc] = true
+
+	var s := PackedStringArray()
+	s.resize(ucs.size())
+	s.fill("?")
+
+	(
+		StateVars
+		. db_saves
+		. query_with_bindings(
+			(
+				"""
+				select code, dwidth, bb_x, bb_y, off_x, off_y, img
+				from font_%s
+				where code in (%s)
+				"""
+				% [StateVars.font.id, ",".join(s)]
+			),
+			ucs.keys()
+		)
+	)
+
+	for q in StateVars.db_saves.query_result:
+		q.tex = (
+			ImageTexture.create_from_image(Util.bits_to_alpha(q.img, q.bb_x, q.bb_y))
+			if q.img
+			else null
+		)
+		cache[q.code] = q
+
+	queue_redraw()
---
+++ components/preview/preview.gd
@@ -0,0 +1,78 @@
+class_name Preview
+extends PanelContainer
+
+@export var window: Window
+@export var presets: OptionButton
+@export var input: TextEdit
+@export var out: PreviewOut
+@export var scroll_out: ScrollContainer
+
+var preset_tree := {
+	"ALPHANUM":
+	[
+		"hamburgefontsiv",
+		"grumpy wizards",
+		"boxing wizards",
+		"weird a-z",
+		"kerning",
+		"briem",
+		"stephenson blake",
+		"kern king",
+		"kern a-z",
+		"min kern pairs",
+		"nick",
+		"numbers",
+	],
+	"LATIN":
+	[
+		"latin",
+		"latin word vomit",
+		"apostrophes",
+		"diacritics a-z",
+		"diacritics lang",
+		"diacritics pseudo",
+		"diacritics extras",
+		"news headlines",
+	],
+	"PROGRAMMING":
+	[
+		"programming",
+		"apl",
+	],
+}
+
+
+func _ready() -> void:
+	window.hide()
+
+	presets.add_separator("PRESETS")
+	for k in preset_tree.keys():
+		presets.add_separator(k)
+		for p in preset_tree[k]:
+			presets.add_item(p)
+	presets.select(0)
+
+	window.close_requested.connect(
+		func():
+			window.hide()
+			out.cache.clear()
+	)
+	presets.item_selected.connect(preset)
+	input.text_changed.connect(preview.bind(false))
+
+
+func preset(i: int) -> void:
+	var file := FileAccess.open(
+		"res://assets/preview/%s.txt" % presets.get_item_text(i), FileAccess.READ
+	)
+	input.text = file.get_as_text()
+	preview()
+
+
+func preview(hard := false) -> void:
+	out.text = input.text
+	input.set_v_scroll(0)
+	scroll_out.set_v_scroll(0)
+	out.refresh(hard)
+	window.popup()
+	input.grab_focus()
