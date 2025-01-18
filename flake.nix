b1b3578749d7c3173f3c1c56a414e0012e23d19e
molarmanful <ben@benpa.ng>
2025-01-17 23:53:34 -0500

    Revert "?"
    This reverts commit 0f97db550dabda39f9eb2f7f202c62fe4bc421c5.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
       in
       {
         devShell = pkgs.mkShell {
