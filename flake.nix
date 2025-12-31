4eddfd5f883c2fccba34cd7161287d9465b5f9b5
molarmanful <ben@benpa.ng>
2025-12-31 15:10:48 -0500

    feat(fmt): statix, deadnix

--- flake.nix
+++ flake.nix
@@ -128,6 +128,8 @@
             projectRootFile = "flake.nix";
             programs = {
               nixfmt.enable = true;
+              statix.enable = true;
+              deadnix.enable = true;
               gdformat = {
                 enable = true;
                 includes = [ "godot/components/**/*.gd" ];
